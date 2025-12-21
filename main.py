68f547453d59a3e3d70330b7fab0d266d0041f06
Melina <qzc0324@gmail.com>
2025-12-20 22:01:27 -0500

    Day 1.1: add /hellp POST endpoint

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from pydantic import BaseModel
+
+class HelloRequest(BaseModel):
+    name: str
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +10,9 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+from fastapi import Body
+
+@app.post("/hello")
+def hello(request: HelloRequest):
+    return {"message": f"Hello, {request.name}!"}
