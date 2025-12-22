11a72966040bf04af359978b657030655a8f238b
Melina <qzc0324@gmail.com>
2025-12-21 22:29:18 -0500

    Day 2: add database setup and User model

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from fastapi import FastAPI
+
 from pydantic import BaseModel
+
+import models
+from database import engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 class HelloRequest(BaseModel):
     name: str
-
-from fastapi import FastAPI
-
-app = FastAPI()
 
 @app.get("/ping")
 def ping():
@@ -16,3 +21,22 @@ from fastapi import Body
 @app.post("/hello")
 def hello(request: HelloRequest):
     return {"message": f"Hello, {request.name}!"}
+
+
+from database import SessionLocal
+from models import User
+@app.get("/test-db")
+def test_db():
+    db = SessionLocal()
+
+    user = User(
+        email=f"test{db.query(User).count()+1}@example.com",
+        hashed_password="notreallyhashed"
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+
+    return {"id": user.id, "email": user.email}
