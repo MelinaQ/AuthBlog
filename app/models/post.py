b6efe91cdd9c39cc90dbcb57edb621a736b9efb0
Melina <qzc0324@gmail.com>
2026-01-11 16:30:26 -0500

    Day 7: add Post model with user relationship

---
+++ app/models/post.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    content = Column(Text, nullable=False)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="posts")
